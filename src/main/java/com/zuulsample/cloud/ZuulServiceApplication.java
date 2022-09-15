6789a3486cfedaf6295c48183bfa6200d9bcb98e
aparna <aparna.06cs03@gmail.com>
2022-09-16 00:47:09 +0530

    changes

--- src/main/java/com/zuulsample/cloud/ZuulServiceApplication.java
+++ src/main/java/com/zuulsample/cloud/ZuulServiceApplication.java
@@ -2,10 +2,12 @@ package com.zuulsample.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
